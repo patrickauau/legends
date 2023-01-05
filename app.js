1db6bfe4210857c05d7f4d28348e46dc8a57d35b
patrickauau <99296414+patrickauau@users.noreply.github.com>
2023-01-05 20:46:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function funcaoBraba(){
+    let div = document.getElementById("imagemBraba")
+    div.style.visibility = 'visible'
+
+    
+
+}
+
+function funcaoBraba1(){
+
+    let div = document.getElementById("importante")
+    div.style.visibility = 'visible'
+
+    let video = document.getElementById("importante")
+    video.play()
+
+
+}
